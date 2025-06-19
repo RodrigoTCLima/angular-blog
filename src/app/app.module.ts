202c5c76a0e73dd79edc4e89f5595c3bd53a19f6
Rodrigo Lima <rodrigotiagocl@gmail.com>
2025-06-19 20:25:12 -0300

    atualização para agregar o novo componente e a nova página ao projeto

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -9,6 +9,8 @@ import { BigCardComponent } from './components/big-card/big-card.component';
 import { SmallCardComponent } from './components/small-card/small-card.component';
 import { HomeComponent } from './pages/home/home.component';
 import { ContentComponent } from './pages/content/content.component';
+import { AboutComponent } from './pages/about/about.component';
+import { MenuFooterComponent } from './components/menu-footer/menu-footer.component';
 
 
 @NgModule({
@@ -19,7 +21,9 @@ import { ContentComponent } from './pages/content/content.component';
     BigCardComponent,
     SmallCardComponent,
     HomeComponent,
-    ContentComponent
+    ContentComponent,
+    AboutComponent,
+    MenuFooterComponent
   ],
   imports: [
     BrowserModule,
